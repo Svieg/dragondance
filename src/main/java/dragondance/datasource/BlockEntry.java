6f76649dc567a1453778f10571df81a9e5d08a8c
Hugo Genesse <svieg.ashcasce@gmail.com>
2023-08-31 13:49:14 -0400

    Add hitCount attribute to BlockEntry class

--- src/main/java/dragondance/datasource/BlockEntry.java
+++ src/main/java/dragondance/datasource/BlockEntry.java
@@ -5,12 +5,14 @@ public class BlockEntry {
 	private int size;
 	private int mid;
 	private int instCount;
+	private int hitCount;
 	
-	public BlockEntry(int entryOffset, int entrySize, int moduleId, int instructionCount) {
+	public BlockEntry(int entryOffset, int entrySize, int moduleId, int instructionCount, int hitCount) {
 		this.offset=entryOffset;
 		this.size=entrySize;
 		this.mid=moduleId;
 		this.instCount=instructionCount;
+		this.hitCount=hitCount;
 	}
 	
 	public final int getOffset() {
@@ -27,5 +29,9 @@ public class BlockEntry {
 	
 	public final int getInstructionCount() {
 		return this.instCount;
+	}
+	
+	public final int getHitCount() {
+		return this.hitCount;
 	}
 }
